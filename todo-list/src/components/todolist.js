318c224985b388b037a6ab616954c2d92637953c
kevin.ramos1229@gmail.com <cait02@CA-IT-01.local>
2021-03-29 15:14:06 -0700

    new css

--- todo-list/src/components/todolist.js
+++ todo-list/src/components/todolist.js
@@ -14,19 +14,29 @@ const TodoList = (props) => {
     //set up state from app.js
     let content = (
         <div>
-            <ul>
-                <div className="main">
-                    <div className="main-menu">MENU</div>
-                    <div className="main-content">
-                        <CreateTask />
-                        <ShowListItem />
-
+            <div className="main">
+                <div className="main-menu">
+                    <div className="menu-boxes">
+                        <div className="btn-box">
+                            <button>ADD DEPARTMETN</button>
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <div className="footer-box">
+                            <button>Add Department</button>
+                        </div>
                     </div>
 
                 </div>
+                <div className="main-content">
+                    <CreateTask />
+                    <ShowListItem />
+
+                </div>
+
+            </div>
 
 
-            </ul>
         </div>
     )
     return content
