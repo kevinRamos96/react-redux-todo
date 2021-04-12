6a7c446bea583379483dd1f392d61ededf19b3e2
kevin.ramos1229@gmail.com <cait02@CA-IT-01.local>
2021-04-12 14:27:44 -0700

    server

--- todo-list/src/redux/store.js
+++
@@ -1,4 +0,0 @@
-import { createStore, applyMiddleware } from "redux"
-import todoReducer from "./reducers/todoReducer"
-import thunk from "redux-thunk"
-export default createStore(todoReducer, applyMiddleware(thunk));
